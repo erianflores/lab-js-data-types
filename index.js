15539fac1b4f7177b172eda26c1bbe3d15ae1a46
Erian Flores <erian.flores@gmail.com>
2024-09-05 20:11:06 +0200

    Changes done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const concat = s1 + ' ' + s2 + ' ' + s3 + ' ' + s4 + ' ' + s5 + ' ';
 
 // Print out the concatenated string
-
+console.log(concat);
 
 
 
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const cap1 = part1.slice(0, part1.length - 1) + part1[part1.length - 1].toUpperCase();
+const cap2 = part2.slice(0, part2.length - 1) + part2[part2.length - 1].toUpperCase();
+const concat2 = cap1 + cap2;
 
 // Print the cameLtaiL-formatted string
-
+console.log(concat2);
 
 
 
@@ -36,9 +38,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let tipPercent = 15;
+let tipAmount = billTotal * tipPercent / 100;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +51,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber =Math.ceil(Math.random() * 10);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
